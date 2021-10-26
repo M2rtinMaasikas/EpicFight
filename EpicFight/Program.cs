e4e70e94d6ec0197d41bb60163ee04d22b516679
TTHK\opilane <zakharmakar1@gmail.com>
2021-10-26 09:56:19 +0300

    1

---
+++ EpicFight/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace EpicFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hero = PickHero();
+            string villain = PickVillain();
+            int heroHP = GenerateHP(hero);
+            int villainHP = GenerateHP(villain);
+            Console.WriteLine($"{hero} will fight {villain}.");
+
+            string heroWeapon = PickWeapon();
+            string villainWeapon = PickWeapon();
+            Console.WriteLine($"{hero} picked {heroWeapon}. {villain} picked {villainWeapon}.");
+        }
+        private static int Hit(string CharacterOne, string CharacterTwo, string someWeapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, someWeapon.Length / 2);
+
+            Console.WriteLine($"{CharacterOne} hit {strike}.");
+            if(strike == someWeapon.Length / 2)
+            {
+                Console.WriteLine($"Awesome! {CharacterOne} made a critcal hit!");
+            }
+            else if (strike == 0)
+            {
+                Console.WriteLine($"{CharacterTwo} dodged the attack!");
+            }
+
+        }
+
+        private static int GenerateHP(string someName)
+        {
+            Random rnd = new Random();
+            return rnd.Next(someName.Length, someName.Length + 10);
+        }
+
+        private static string PickWeapon()
+        {
+            string[] weapon = { "plastic fork", "banana", "frying pan", "purse", "flip-flop" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, weapon.Length);
+
+            return weapon[randomIndex];
+        }
+
+        private static string PickHero()
+        {
+            string[] superHeroes = { "Luke Skywalker", "Batman", "Spiderman", "Patric", "Lara Croft" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, superHeroes.Length);
+
+            return superHeroes[randomIndex];
+        }
+
+        private static string PickVillain()
+        {
+            string[] villains = { "Darth Vader", "Joker", "Voldemort", "T-1000", "Agent Smith" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, villains.Length);
+
+            return villains[randomIndex];
+        }
+
+
+    }
+}
